d077208cde60916c035e905e0e7c8a02006c975c
novaavon <novaavonofficial@gmail.com>
2025-06-23 03:12:59 +0800

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+toggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
